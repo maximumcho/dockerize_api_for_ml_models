d5e4a3bf19d39014a77727b0ab794583d2a24dcc
Amber Cho <maximumcho@gmail.com>
2023-02-25 12:15:30 +0800

    First commit for initial project.

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# syntax=docker/dockerfile:1
+
+# start by pulling the python image
+FROM python:3.8-bullseye
+
+# switch working directory
+WORKDIR /docker-image
+
+# copy the requirements file into the image
+COPY requirements.txt /docker-image
+
+# install the dependencies and packages in the requirements file
+RUN pip3 install -r requirements.txt
+
+# copy every content from the local file to the image
+COPY . /docker-image
+
+# Add this to avoid "Error: Could not locate a Flask application. Use the 'flask --app' option, 'FLASK_APP' environment variable, or a 'wsgi.py' or 'app.py' file in the current directory"
+ENV FLASK_APP=api.py
+
+CMD ["python3", "-m" , "flask", "run", "--host=0.0.0.0"]
---
+++ test_api.py
@@ -0,0 +1,30 @@
+import numpy as np
+import requests
+import json
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+# Load data
+iris = load_iris()
+
+#print("iris: " + str(iris))
+
+# Split into train and test sets using the same random state
+X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=12)
+
+# Serialize the data into json and send the request to the model
+xTest = json.dumps(X_test.tolist());
+payload = {'data': xTest}
+# Fix the single quote bug
+payload = json.dumps(payload)
+
+#print(payload)
+
+# Set request's header.
+headers = {'Content-type': 'application/json'}
+y_predict = requests.post('http://localhost:5000/iris/predict', headers=headers, data=payload)
+
+# Make array from the list
+y_predict = np.array(y_predict.json())
+print(y_predict)
